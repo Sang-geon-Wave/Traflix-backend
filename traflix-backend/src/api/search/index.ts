a8c4f3762aae51055495975cde801793af2874d0
parkjs82 <54925443+parkjs82@users.noreply.github.com>
2023-09-25 13:00:15 +0900

    feat search api

---
+++ traflix-backend/src/api/search/index.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response, Router } from 'express';
+import promisePool from '../../db';
+import { authProtected, authUnprotected } from '../../middlewares/auth';
+import HttpStatus from 'http-status-codes';
+
+const router: Router = express.Router();
+
+router.post(
+  '/stationName',
+  authUnprotected,
+  async (req: Request, res: Response) => {
+    const [rows, _] = await promisePool.execute(
+      'SELECT station_name, station_code FROM traflix.STATION ORDER BY station_name;',
+    );
+
+    console.log(rows, _);
+
+    return res.status(HttpStatus.OK).json({
+      status: HttpStatus.OK,
+      message: 'station name query success',
+      data: rows,
+    });
+  },
+);
+
+module.exports = router;
